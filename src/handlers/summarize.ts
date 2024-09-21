4254f03f0353438b48a3911df37fd0a962cbac0c
chandradeepc <chandradeep@perfectrec.com>
2024-09-21 13:59:11 -0700

    added anthropic and firecrawl search integration

--- src/handlers/summarize.ts
+++ src/handlers/summarize.ts
@@ -1,5 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { LlamaParseReader } from "llamaindex";
+import Anthropic from "@anthropic-ai/sdk";
+import FirecrawlApp from "@mendable/firecrawl-js";
 import "dotenv/config";
 import fs from "fs-extra";
 import path from "path";
@@ -13,6 +15,23 @@ interface SummarizeRequestParams {
     | "tablesAndFiguresOnly"
     | "ultimate";
 }
+
+type Model = "claude-3-5-sonnet-20240620";
+
+const ANTHROPIC_TEMPERATURE = 0;
+const ANTHROPIC_MODEL = "claude-3-5-sonnet-20240620";
+
+const firecrawl = new FirecrawlApp({
+  apiKey: process.env.FIRECRAWL_API_KEY,
+});
+
+const anthropic = new Anthropic({
+  apiKey: process.env.ANTHROPIC_API_KEY,
+  defaultHeaders: {
+    "anthropic-beta":
+      "prompt-caching-2024-07-31,max-tokens-3-5-sonnet-2024-07-15",
+  },
+});
 
 export default async function handler(
   request: FastifyRequest<{
@@ -48,4 +67,50 @@ export default async function handler(
   //await fs.remove(tempFilePath);
 
   return { message: "Processed file", documents, images };
+}
+
+async function getAnthropicCompletion(
+  systemPrompt: string,
+  userPrompt: string,
+  model: string,
+  temperature: number
+) {
+  const completion = await anthropic.messages.create({
+    max_tokens: 8192,
+    system: [{ type: "text", text: systemPrompt }],
+    model: model,
+    temperature: temperature,
+    messages: [
+      {
+        role: "user",
+        content: userPrompt,
+      },
+    ],
+  });
+
+  const completionText = (completion.content[0] as Anthropic.TextBlock).text;
+  return completionText;
+}
+
+async function getWebContext(
+  link: string
+): Promise<{ url: string; markdown: string }[]> {
+  const result = await firecrawl.search(link);
+
+  if ("data" in result) {
+    const data: { url: string; markdown: string }[] = [];
+
+    result.data.forEach((item: { url: string; markdown: string }) => {
+      const cleanedMarkdown = item.markdown
+        .replace(/<br\s*\/?>/gi, "")
+        .replace(/\[.*?\]\(.*?\)/g, "")
+        .replace(/\s{2,}/g, " ")
+        .replace(/\n{2,}/g, "\n");
+      data.push({ url: item.url, markdown: cleanedMarkdown });
+    });
+
+    return data;
+  } else {
+    throw new Error("Failed to scrape content");
+  }
 }
