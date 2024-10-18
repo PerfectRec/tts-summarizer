cad3fdd37fec6c760acbc71066e3e5540b10812a
chandradeepc <chandradeep@perfectrec.com>
2024-10-18 11:20:03 -0700

    client updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import r2 from "./assets/r2.jpeg";
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
@@ -64,8 +65,8 @@ function App() {
   };
 
   return (
-    <div className="max-w-lg mx-auto">
-      <h1 className="text-4xl font-bold mb-4">paper2audio</h1>
+    <div className="max-w-xl mx-auto">
+      <h1 className="text-5xl font-bold my-4">paper2audio</h1>
       <div className="mb-4 text-sm">
         paper2audio creates an audio version of your research paper PDF. It
         narrates all the text of the paper, plus AI-generated summaries of any
@@ -111,13 +112,185 @@ function App() {
         {errorMessage && (
           <div className="text-sm text-red-500">{errorMessage}</div>
         )}
-        <div className="text-sm text-red-500">
+        <div className="text-sm">
           This is a new project that we’re actively improving. The audio output
           is generally good, but not perfect. Math heavy papers unfortunately do
           not naturally translate well to audio. AI-generated summaries may not
           be 100% accurate.
         </div>
       </form>
+      <div className="mt-16">
+        <h2 className="text-2xl font-bold mb-2">About</h2>
+        <p className="">
+          paper2audio is a new project recently started by
+          <a
+            href="https://www.linkedin.com/in/jgolden9/"
+            className="text-blue-500"
+          >
+            {" "}
+            Joe Golden
+          </a>{" "}
+          and
+          <a
+            href="https://www.linkedin.com/in/chandradeepc/"
+            className="text-blue-500"
+          >
+            {" "}
+            Chandradeep Chowdhury
+          </a>
+          . Its goal: completely or partially replace reading a research paper.
+        </p>
+        <h3 className="text-xl font-bold mt-8 mb-2">Some use cases:</h3>
+        <ul className="list-disc list-outside text-left ml-4 space-y-2">
+          <li>
+            You never get to the bottom of your stack of research papers to
+            read. Listen to them instead.
+          </li>
+          <li>
+            You have a paper to review. Listen to it first, then review it more
+            quickly and easily.
+          </li>
+          <li>
+            You find it hard to stay focused on reading research papers. Listen
+            instead.
+          </li>
+        </ul>
+        <h3 className="text-xl font-bold mt-8">How to listen to a paper</h3>
+        <p className="mt-2">
+          We recommend{" "}
+          <a href="https://www.videolan.org/" className="text-blue-500">
+            {" "}
+            VLC Media Player
+          </a>
+          , which is free, open-source and cross-platform.{" "}
+          <a
+            href="https://apps.apple.com/us/app/vlc-media-player/id650377962"
+            className="text-blue-500"
+          >
+            {" "}
+            iOS
+          </a>{" "}
+          <a
+            href="https://play.google.com/store/apps/details?id=org.videolan.vlc&hl=en_US"
+            className="text-blue-500"
+          >
+            {" "}
+            Android
+          </a>
+        </p>
+        <h3 className="text-xl font-bold mt-8 mb-2">
+          Joe’s advice for listening
+        </h3>
+        <ul className="list-disc list-outside ml-4 text-left space-y-2">
+          <li>
+            Find your optimal playback speed overall. 1.5-2x or possibly even
+            higher may be best for you. Too slow of a speed may be boring for
+            you and make it hard to pay attention. A faster speed helps you
+            listen to more papers.
+          </li>
+          <li>
+            Adjust your speed for some papers or sections. You might need to
+            slow down to understand dense information or for information you
+            need to understand very well. Conversely, you might like to speed up
+            for less dense or critical information.
+          </li>
+          <li>
+            Consider listening to important papers or parts of papers a second
+            time.
+          </li>
+          <li>
+            Try slowing increasing your default playback speed over time as you
+            get more experienced and used to listening to papers.
+          </li>
+          <li>
+            Listen when doing things that require your time, but not your full
+            attention. Here are some good activities that support listening:
+            driving, doing dishes or laundry, cleaning, walking, traveling,
+            exercising, getting dressed, brushing your teeth and yard work.
+          </li>
+        </ul>
+        <h3 className="text-xl font-bold mt-8 mb-2">
+          Next steps for paper2audio
+        </h3>
+        <ul className="list-disc list-outside ml-4 text-left space-y-2">
+          <li>Improve various aspects of audio output</li>
+          <li>
+            Add some customization options (while keeping a focus on optimizing
+            the default output)
+          </li>
+          <li>Improvements and features requested by users</li>
+          <li>Building a phone app with integrated playback</li>
+        </ul>
+        <h3 className="text-xl font-bold mt-8 mb-2">
+          Some issues we’ve already addressed
+        </h3>
+        <ul className="list-disc list-outside ml-4 text-left space-y-2">
+          <li>Added 2 column paper support</li>
+          <li>
+            Implemented detecting and removing meta information before the
+            abstract including journal logos
+          </li>
+          <li>
+            Cleaning up and rewriting author info which is formatted poorly for
+            text to speech
+          </li>
+          <li>
+            Repositioning tables and figures near their first mention for better
+            audio flow
+          </li>
+          <li>
+            Getting rid of meta info throughout the paper like page numbers,
+            disclaimers, and table of contents
+          </li>
+        </ul>
+        <h3 className="text-xl font-bold mt-8">
+          Our Sci-Fi Inspiration from The Matrix
+        </h3>
+        <iframe
+          className="w-full aspect-video mt-4"
+          src="https://www.youtube.com/embed/w_8NsPQBdV0"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <h3 className="text-xl font-bold mt-8">Personal note from Joe</h3>
+        <p className="text-left mt-2">
+          I’m a 39 year old economics PhD and entrepreneur. I’ve always had too
+          much to read since I was a teenager. I often find it hard to focus
+          when sitting down to read, but I also get insanely bored doing chores
+          and other low attention activities. For me, listening kills two birds
+          with one stone.
+        </p>
+        <p className="text-left mt-4">
+          Like many people, I’ve naturally gravitated towards audiobooks and
+          podcasts. More unusually, for about a decade, I’ve saved news articles
+          to an app, and used text-to-speech to listen to them. That’s how over
+          time I’ve developed the above advice for listening. Before that, my
+          browser tabs from news sites would continually pile up, forcing me to
+          cyclically declare tab bankruptcy.
+        </p>
+        <p className="text-left mt-4">
+          But, until recent advances in generative AI, the underlying technology
+          didn’t exist to be able to listen to research papers that include
+          non-text content like tables and figures. So, despite my interest, due
+          to a scarcity of both time and attention, I’ve struggled to read as
+          many papers as I would like. So, I started paper2audio to solve this
+          problem for myself, and I’m hoping lots of other people who similarly
+          would be interested in listening to research papers.
+        </p>
+        <h3 className="text-xl font-bold mt-8">We Want Your Feedback</h3>
+        <p className="text-left mt-2">
+          We’ll use it to make paper2audio a better service! Please share
+          feedback:
+          <a href="mailto:Joe@paper2audio.com" className="text-blue-500">
+            {" "}
+            Joe@paper2audio.com
+          </a>
+          . Be critical and channel your inner R2:
+        </p>
+        <img src={r2} alt="R2 meme graphic" className="my-8" />
+      </div>
     </div>
   );
 }
